d4696706326ff515c76e33f49abcd5956a4be284
hsix <shinhyeonsik@gmail.com>
2020-07-20 10:01:55 +0900

    다음 순열 진행중

---
+++ 10972_다음 순열/10972.py
@@ -0,0 +1,12 @@
+def main():
+    import sys
+    r = lambda: sys.stdin.readline()
+
+    n = int(r().strip())
+    d = list(range(1, n+1))
+    next_permu(d)
+
+def next_permu(d):
+
+if __name__ == '__main__':
+    main()
